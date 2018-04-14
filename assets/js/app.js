0f67414779f402477a192dcbee14a8210ad4b6fa
Lucyloules <lucia.lagos@gmail.com>
2018-04-14 10:49:18 -0300

    creo archivo funcionalidad juego DollyGame

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   console.log('ready!');
   // creo los botones con las letras
-  var abc = 'abcdefghij';
-  for (i = 0; i < letras.length; i++) {}
+  const abc = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,u,v,w,x,y,z' ;
+  for (i = 0; i < abc.length; i++) {
+    Console.log(abci]);
+  }
 });
