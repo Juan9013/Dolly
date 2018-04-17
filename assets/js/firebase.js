6319703e6771c88c7234ed8376be6109a7d9a73f
Lucyloules <lucia.lagos@gmail.com>
2018-04-17 17:16:46 -0300

    firebase instalado con npm

---
+++ assets/js/firebase.js
@@ -0,0 +1,10 @@
+// configuración inicial de Firebase
+var config = {
+  apiKey: 'AIzaSyDGnfXO-EUofIBKpRXxHGMccHBMWMoN1kE',
+  authDomain: 'dollygame-c535f.firebaseapp.com',
+  databaseURL: 'https://dollygame-c535f.firebaseio.com',
+  projectId: 'dollygame-c535f',
+  storageBucket: 'dollygame-c535f.appspot.com',
+  messagingSenderId: '160765417030'
+};
+firebase.initializeApp(config);
